becdfa55f9114a3d8b318d1243be03b4a5fbd496
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-11-03 12:17:27 +0300

    Task-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int desiredAmountOfSavings = 2_459_000;
+        short monthlySavings = 15_000;
+        int amountOfSavings = 0;
+        short countOfMonths = 0;
+
+        while (amountOfSavings <= desiredAmountOfSavings) {
+            countOfMonths++;
+            amountOfSavings = amountOfSavings + monthlySavings;
+        }
+        System.out.println("Месяц " + countOfMonths + ", сумма накоплений равна " + amountOfSavings + " рублей");
     }
 }
